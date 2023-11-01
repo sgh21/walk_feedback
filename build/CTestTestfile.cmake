847285b1c31bd195a3e842c09329118103144ead
申广辉 <sgh21@mails.tsinghua.edu.cn>
2023-11-01 15:46:26 +0800

    init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sgh/workspace/walk_feedback/src
+# Build directory: /home/sgh/workspace/walk_feedback/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/sgh/workspace/walk_feedback/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/sgh/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/sgh/catkin_ws/devel;/opt/ros/noetic' != '' else []
